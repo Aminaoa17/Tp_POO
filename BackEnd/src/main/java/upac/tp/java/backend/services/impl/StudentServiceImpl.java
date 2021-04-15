932227279e93f54a509b6574481f001db32ce5cc
Meudje <ryankarl65@yahoo.fr>
2021-04-15 20:57:27 +0200

    Feat - add student service

---
+++ BackEnd/src/main/java/upac/tp/java/backend/services/impl/StudentServiceImpl.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package upac.tp.java.backend.services.impl;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import upac.tp.java.backend.repository.StudentRepositoryInterface;
+import upac.tp.java.backend.services.StudentService;
+
+/**
+ *
+ * @author ryank
+ */
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    private final StudentRepositoryInterface studentQuery;
+
+    public StudentServiceImpl(@Qualifier("StudentRepositoryInterface") StudentRepositoryInterface studentQuery) {
+        this.studentQuery = studentQuery;
+    }
+
+}
